cf38afa5cd73cd180153102d08d8fa8393ac4f23
unfriendme <100465853+Unfriendme-Turrext@users.noreply.github.com>
2022-06-27 18:52:52 +0500

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -21,22 +21,24 @@ RegisterNetEvent('unfriendme_scripts:additem', function(ItemName)
   end
 end)
 RegisterNetEvent('unfriendme_scripts:process', function(ItemName , ItemName2)
-	local source = source
-	local xPlayer = ESX.GetPlayerFromId(source)
-	local xItem = xPlayer.getInventoryItem(ItemName)
-	local ped = GetPlayerPed(source)
-	local playerCoords = GetEntityCoords(ped)
-	if #(v.Process - playerCoords) < 30   then
-	if xItem.count > 1 then
-		if xPlayer.canCarryItem(ItemName2, 1) then
-			xPlayer.removeInventoryItem(ItemName, 2)
-			xPlayer.addInventoryItem(ItemName2, 1)
-			xPlayer.showNotification('You have recieved 1x' .. ItemName)
+	for k, v in pairs(Config.Drugs) do
+		local source = source
+		local xPlayer = ESX.GetPlayerFromId(source)
+		local xItem = xPlayer.getInventoryItem(ItemName)
+		local ped = GetPlayerPed(source)
+		local playerCoords = GetEntityCoords(ped)
+		if #(v.Process - playerCoords) < 30   then
+		if xItem.count > 1 then
+			if xPlayer.canCarryItem(ItemName2, 1) then
+				xPlayer.removeInventoryItem(ItemName, 2)
+				xPlayer.addInventoryItem(ItemName2, 1)
+				xPlayer.showNotification('You have recieved 1x' .. ItemName)
+			else
+				xPlayer.showNotification('You cannot hold ' .. ItemName)
+			end
 		else
-			xPlayer.showNotification('You cannot hold ' .. ItemName)
+			xPlayer.showNotification("You don't have 2x " .. ItemName)
 		end
-	else
-		xPlayer.showNotification("You don't have 2x " .. ItemName)
 	end
    end
 end)
